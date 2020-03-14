de5ca37baa06bdbe62ea8eb91f3fd3f44246a844
Abhishek Shukla <shuklaravidob@gmail.com>
2020-03-14 13:27:09 +0530

    import module datepicker is not correct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import {FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NewCmpComponent} from './new-cmp/new-cmp.component';
-import {AlertModule,ButtonsModule } from 'ngx-bootstrap';
-
+import {AlertModule,ButtonsModule} from 'ngx-bootstrap';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {BsDropdownModule} from 'ngx-bootstrap';
+import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +17,8 @@ import {AlertModule,ButtonsModule } from 'ngx-bootstrap';
     BrowserModule,
     FormsModule,
     ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
